0c76b1ddb169519255cc6af281c2fe334cfcda89
Vladosya <jvladosyans@gmail.com>
2022-01-16 12:05:38 +0300

    first commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+extern crate stopwatch;
+use chrono::prelude::*;
 use std::cmp::Ordering::{Equal, Greater, Less};
 use std::collections::HashMap;
 use std::fs::File;
@@ -5,6 +7,7 @@ use std::fs::{self, metadata};
 use std::io::{self, Read};
 use std::io::{Seek, Write};
 use std::path::{Path, PathBuf};
+use stopwatch::Stopwatch;
 use wkhtmltopdf::PdfApplication;
 use zip::read::ZipFile;
 use zip::result::ZipResult;
@@ -18,7 +21,7 @@ async fn get_zip(need_url: &String) -> anyhow::Result<String> {
     let path: &Path = Path::new(&file_name);
     let mut file: File = match File::create(&path) {
         Ok(file) => file,
-        Err(err) => panic!("Error create file --> {}", err),
+        Err(err) => panic!("Error create file in get_zip func ---> {}", err),
     };
 
     file.write_all(&download_zip)?;
@@ -35,6 +38,16 @@ where
     (0..archive.len())
         .map(|i| archive.by_index(i).and_then(|file| browse_func(&file)))
         .collect()
+}
+
+async fn remove_need_file(name_file: &str) {
+    match Path::new(name_file).exists() {
+        true => match fs::remove_file(name_file) {
+            Ok(res) => res,
+            Err(err) => panic!("Error remove file {} {}", name_file, err),
+        },
+        false => (),
+    };
 }
 
 // извлечение из zip архива
@@ -90,13 +103,53 @@ async fn extract_from_zip() -> String {
 
 // конвертировать файл в pdf:
 async fn file_to_pdf(name_folder: String) {
+    let mut sw: Stopwatch = Stopwatch::start_new();
     let pdf_app: PdfApplication = PdfApplication::new().expect("Failed to init PDF application");
     let mut pdfout: wkhtmltopdf::PdfOutput = pdf_app
         .builder()
         .build_from_path(format!("{}/index.html", name_folder))
         .expect("failed to build pdf");
 
-    pdfout.save("result.pdf").expect("failed to save foo.pdf");
+    let file_pdf_name: &str = "result.pdf";
+
+    pdfout.save(file_pdf_name).expect("failed to save foo.pdf");
+    sw.stop();
+    match Path::new(file_pdf_name).exists() {
+        true => {
+            let mut perform_file: File = match File::create("perform_operation.txt") {
+                Ok(perform_file) => perform_file,
+                Err(err) => panic!("Error create file perform_operation.txt ---> {}", err),
+            };
+            let local: DateTime<Local> = Local::now();
+            let local_date: String = match local.date().month().to_string().len() {
+                1 => {
+                    format!(
+                        "{}:0{}:{}",
+                        local.date().day(),
+                        local.date().month(),
+                        local.date().year()
+                    )
+                }
+                _ => {
+                    format!(
+                        "{}:{}:{}",
+                        local.date().day(),
+                        local.date().month(),
+                        local.date().year()
+                    )
+                }
+            };
+            match write!(
+                &mut perform_file,
+                "LOG OF EXECUTED OPERATION: \n File to convert: {}index.html \n Name file: {} \n Date of operation: date: {} time: {} \n Time spent converting: {} milliseconds",
+                name_folder, file_pdf_name, local_date, &local.to_string()[11..19].trim(), sw.elapsed_ms()
+            ) {
+                Ok(file) => file,
+                Err(err) => panic!("Error create file {}", err),
+            };
+        }
+        false => println!("Error"),
+    }
     fs::remove_dir_all(format!("{}", name_folder)).unwrap();
 }
 
@@ -104,13 +157,16 @@ async fn file_to_pdf(name_folder: String) {
 async fn check_file_zip(need_path_download: &String) {
     // проверка зип архива
     let res_get_zip: String = match get_zip(&need_path_download).await {
-        Ok(res) => res,
-        Err(err) => panic!("Error {}", err),
+        Ok(res_get_zip) => res_get_zip,
+        Err(err) => panic!(
+            "Function call get_zip is error in check_file_zip ---> {}",
+            err
+        ),
     };
 
     let need_size: u64 = 2147483648;
-    if let Ok(meta) = metadata("src/download.zip") {
-        let _size = match meta.len().cmp(&need_size) {
+    if let Ok(meta) = metadata(&res_get_zip) {
+        match meta.len().cmp(&need_size) {
             Less => {
                 println!(
                     "Folder name {}, size is {} is normal",
@@ -119,25 +175,29 @@ async fn check_file_zip(need_path_download: &String) {
                 );
 
                 // ------------------------ Проверка на нужные папки
-                let mut file_info: File =
-                    File::open(&res_get_zip[2..].trim()).expect("Couldn't open file");
+                let mut file_info: File = match File::open(&res_get_zip[2..].trim()) {
+                    Ok(file_info) => file_info,
+                    Err(err) => panic!("Error couldn't open file in check_file_zip ---> {}", err),
+                };
 
                 let folder_info: Vec<String> = match browse_zip_archive(&mut file_info, |f| {
                     Ok(format!("File name --> {}", f.name(),))
                 }) {
-                    Ok(files) => files,
-                    Err(err) => panic!("Error info files {}", err),
+                    Ok(folder_info) => folder_info,
+                    Err(err) => panic!("Error get information to folder ---> {}", err),
                 };
 
                 let father_folder: &str = &folder_info[0][14..];
                 let need_folders: Vec<String> = vec![
                     format!("{}img/", father_folder),
                     format!("{}css/", father_folder),
+                    format!("{}style/", father_folder),
                     format!("{}index.html", father_folder),
                 ];
                 let mut list_hash: HashMap<String, bool> = HashMap::new();
                 list_hash.insert(String::from("img"), false);
                 list_hash.insert(String::from("css"), false);
+                list_hash.insert(String::from("style"), false);
                 list_hash.insert(String::from("index.html"), false);
 
                 for i in need_folders.iter() {
@@ -167,29 +227,55 @@ async fn check_file_zip(need_path_download: &String) {
 
                 match count {
                     3 => {
-                        println!("The folders that should be in the archive are the same");
-                        // Вызываем ф-цию конвертации в pdf
+                        for i in folder_info {
+                            println!("i --> {}", i);
+                            if i.contains("index.html") {
+                                // Вызываем ф-ции: извлечение из архива zip и после нее конвертация в pdf
+                                println!("The folders that should be in the archive are the same");
+                                let res_extract_from_zip: String = extract_from_zip().await;
+                                file_to_pdf(res_extract_from_zip).await;
+                            } else {
+                                println!("The folders that should be in the archive do not match");
+                                fs::remove_file("./download.zip").unwrap();
+                            }
+                        }
                     }
-                    _ => println!("The folders that should be in the archive do not match"),
+                    4 => {
+                        // Вызываем ф-ции: извлечение из архива zip и после нее конвертация в pdf
+                        println!("The folders that should be in the archive are the same");
+                        let res_extract_from_zip: String = extract_from_zip().await;
+                        file_to_pdf(res_extract_from_zip).await;
+                    }
+                    _ => {
+                        println!("The folders that should be in the archive do not match");
+                        fs::remove_file("./download.zip").unwrap();
+                    }
                 }
             }
-            Greater => println!(
-                "Folder name {}, size  {} more 2 gigabytes memory",
-                &res_get_zip[2..].trim(),
-                meta.len()
-            ),
-            Equal => println!(
-                "Folder name {}, size {} equal 2 gigabytes memory",
-                &res_get_zip[2..].trim(),
-                meta.len()
-            ),
+            Greater => {
+                println!(
+                    "Folder name {}, size  {} more 2 gigabytes memory",
+                    &res_get_zip[2..].trim(),
+                    meta.len()
+                );
+                fs::remove_file("./download.zip").unwrap();
+            }
+            Equal => {
+                println!(
+                    "Folder name {}, size {} equal 2 gigabytes memory",
+                    &res_get_zip[2..].trim(),
+                    meta.len()
+                );
+                fs::remove_file("./download.zip").unwrap();
+            }
         };
     }
 }
+
 async fn start(need_path_download: String) {
+    remove_need_file("perform_operation.txt").await;
+    remove_need_file("result.pdf").await;
     check_file_zip(&need_path_download).await;
-    let res_extract_from_zip: String = extract_from_zip().await;
-    file_to_pdf(res_extract_from_zip).await;
 }
 
 #[tokio::main]
@@ -199,4 +285,54 @@ async fn main() {
             .to_string(),
     )
     .await;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_remove_need_file() {
+        let file_one: &str = "perform_operation.txt";
+        let file_two: &str = "result.pdf";
+        remove_need_file(file_one).await;
+        remove_need_file(file_two).await;
+        assert_eq!(Path::new(file_one).exists(), false);
+        assert_eq!(Path::new(file_two).exists(), false);
+    }
+
+    #[tokio::test]
+    async fn test_extract_from_zip() {
+        let need_url: String = "https://github.com/Vladosya/verstka-bem-adaptive-no-mobile/archive/refs/heads/main.zip".to_string();
+        let _res_get_zip: String = match get_zip(&need_url).await {
+            Ok(res) => res,
+            Err(err) => panic!("Error {}", err),
+        };
+        let _res_extract: String = extract_from_zip().await;
+        assert_eq!(Path::new("./download.zip").exists(), false);
+    }
+
+    #[tokio::test]
+    async fn test_file_to_pdf() {
+        let need_url: String = "https://github.com/Vladosya/verstka-bem-adaptive-no-mobile/archive/refs/heads/main.zip".to_string();
+        let _res_get_zip: String = match get_zip(&need_url).await {
+            Ok(res) => res,
+            Err(err) => panic!("Error {}", err),
+        };
+        let res_extract_from_zip: String = extract_from_zip().await;
+        file_to_pdf(res_extract_from_zip).await;
+        assert_eq!(Path::new("./download.zip").exists(), false);
+        assert_eq!(Path::new("result.pdf").exists(), true);
+    }
+
+    #[tokio::test]
+    async fn test_get_zip() {
+        let need_url: String = "https://github.com/Vladosya/verstka-bem-adaptive-no-mobile/archive/refs/heads/main.zip".to_string();
+        let res_get_zip: String = match get_zip(&need_url).await {
+            Ok(res) => res,
+            Err(err) => panic!("Error {}", err),
+        };
+
+        assert_eq!(res_get_zip, "./download.zip".to_string());
+    }
 }
